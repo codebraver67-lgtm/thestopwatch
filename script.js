7832bd15e375e591140fa69f403d1797905990ff
codebraver67-lgtm <codebraver67@codebrave.org>
2025-12-19 19:52:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let part= prompt("enter here")
+if (part>10){
+    console.log("this number is greater than 10");
+}
+else{
+    console.log("less than 10");
+}
+let sec1=0 ;
+let tens1=0;
+let optens = document.getElementById("tens");
+let opsec = document.getElementById("sec");
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let reset = document.getElementById("reset");
+
+
+
+
+
+
+
